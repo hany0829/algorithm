862389bc45228fdc2194d5ebfc9b4c0570e98b5d
hany0829 <121420592+hany0829@users.noreply.github.com>
2023-01-26 15:43:40 +0900

    [Silver V] Title: 카드1, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/2161. 카드1/카드1.py
+++ 백준/Silver/2161. 카드1/카드1.py
@@ -1,6 +1,8 @@
-dc=[]
-n=int(input())
-queue=list(range(1,n+1))
+n =int(input())
+queue = (list(range(1,n+1)))
+# [i for i in range(1, N+1)]
+dc = []
+
 while len(queue) !=1:
     dc.append(queue.pop(0))
     queue.append(queue.pop(0))
